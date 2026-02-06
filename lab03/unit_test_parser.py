8b401991123fd498a28d3afcf826be4ea7bdf589
Nehemiah_Sanders <nehitimothy10@gmail.com>
2026-02-06 13:23:32 -0600

    added first test case for parser function

--- lab03/unit_test_parser.py
+++ lab03/unit_test_parser.py
@@ -1,12 +1,21 @@
 # File to test
 def parse_file(self, filename):
-        """
-        Parses a .uabcs file and returns list of record dictionaries.
-        Raises ValueError for invalid file format or extension.
-        Raises FileNotFoundError if file doesn't exist.
-        """
-        pass
+    """
+    Parses a .uabcs file and returns list of record dictionaries.
+    Raises ValueError for invalid file format or extension.
+    Raises FileNotFoundError if file doesn't exist.
+    """
+    pass
 
 
 ############ Unit Tests ############
 
+
+def test_parse_file_invalid_extension(filename):
+    if not filename.endswith(".uabcs"):
+        raise ValueError("Invalid file extension. Expected '.uabcs'")
+
+
+############# Example usage of the test #############
+
+print(test_parse_file_invalid_extension("grades.uabcs"))
