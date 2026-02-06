2c8552ee51a8bf1d4bd80aeda1e566558665b4f3
Nehemiah_Sanders <nehitimothy10@gmail.com>
2026-02-06 12:40:08 -0600

    changed python version

--- lab03/setup_example.py
+++ lab03/setup_example.py
@@ -1,13 +1,13 @@
 # Please import the right on based on your python version
 # e.g. if you have python 3.10.xx use from byte_code.python_310.uabcs_parser import uabcs_parser
 # we have included the byte code for python versions 3.8 - 3.13
-from byte_code.python_38.uabcs_parser import uabcs_parser
+from byte_code.python_312.uabcs_parser import uabcs_parser
 
 # Initialize a parser instance
 parser = uabcs_parser()
 
 # read a .uabcs file using the parser
-data = parser.parse_file('student_grades.uabcs')
+data = parser.parse_file("student_grades.uabcs")
 
 # print the data
 print(data)
