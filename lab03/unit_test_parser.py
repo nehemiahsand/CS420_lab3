db937cbf4b6031c39ffc50d5348baaddec228e13
Nehemiah_Sanders <nehitimothy10@gmail.com>
2026-02-06 13:08:47 -0600

    add files

---
+++ lab03/unit_test_parser.py
@@ -0,0 +1,12 @@
+# File to test
+def parse_file(self, filename):
+        """
+        Parses a .uabcs file and returns list of record dictionaries.
+        Raises ValueError for invalid file format or extension.
+        Raises FileNotFoundError if file doesn't exist.
+        """
+        pass
+
+
+############ Unit Tests ############
+
